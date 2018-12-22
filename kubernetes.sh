946d6efb00feed400f7d4ca61a273d3673a95109
Eugene Antonov <jetman@sysadm.ws>
2018-12-22 16:59:54 +0200

    additions

--- kubernetes.sh
+++ kubernetes.sh
@@ -2,5 +2,5 @@
 
 function registry_login {
 	# make docker config per workspace (instead of gitlab-runner home)
-	docker --config=./docker-config login -u gitlab-ci-token -p $CI_JOB_TOKEN $CI_REGISTRY
+	echo "$CI_JOB_TOKEN" | docker --config=./docker-config login -u "$CI_REGISTRY_USER" "$CI_REGISTRY" --password-stdin
 }
