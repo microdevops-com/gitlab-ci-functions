a34c148469868d7c66f153cf5b1c9069e408d472
Eugene Antonov <jetman@sysadm.ws>
2018-12-22 18:29:11 +0200

    additions

--- kubernetes.sh
+++ kubernetes.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 KUBECTL="kubectl --server=$KUBE_SERVER --token=$KUBE_TOKEN"
+KUBE_NAMESPACE=$(echo $KUBE_NAMESPACE| tr "[:upper:]" "[:lower:]" | sed "s/[^a-zA-Z0-9-]/-/g")
 
 function registry_login {
 	docker login -u "$CI_REGISTRY_USER" -p $CI_JOB_TOKEN $CI_REGISTRY
