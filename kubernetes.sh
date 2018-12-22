f5af70f92ee36f6f1e93782c00b0422fc9777afd
Eugene Antonov <jetman@sysadm.ws>
2018-12-22 18:21:27 +0200

    additions

--- kubernetes.sh
+++ kubernetes.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+KUBECTL="kubectl --server=$KUBE_SERVER --token=$KUBE_TOKEN"
+
 function registry_login {
 	docker login -u "$CI_REGISTRY_USER" -p $CI_JOB_TOKEN $CI_REGISTRY
+}
+
+function ensure_namespace {
+	$KUBECTL describe namespace "$KUBE_NAMESPACE" || $KUBECTL create namespace "$KUBE_NAMESPACE"
 }
