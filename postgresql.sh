bdce31ee4bb4d66fa53374e940149d38d5bdb66f
Eugene Antonov <jetman@sysadm.ws>
2019-03-06 17:41:44 +0200

    echo db name in pg

--- postgresql.sh
+++ postgresql.sh
@@ -3,6 +3,7 @@
 function postgresql_create_db () {
 	local NEW_DB_NAME="$1"
 	# check db exist and if not - create
+	echo "Creating DB: $NEW_DB_NAME"
 	PGPASSWORD="$POSTGRESQL_PASS" psql -h "$POSTGRESQL_HOST" -p "$POSTGRESQL_PORT" -U "$POSTGRESQL_USER" -w -lqt | cut -d \| -f 1 | grep -qw "$NEW_DB_NAME" || \
 		PGPASSWORD="$POSTGRESQL_PASS" createdb -h "$POSTGRESQL_HOST" -p "$POSTGRESQL_PORT" -U "$POSTGRESQL_USER" -w "$NEW_DB_NAME"
 }
