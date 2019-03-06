e120e14fb4e8da0020c84d562c8b53fe13db0737
Eugene Antonov <jetman@sysadm.ws>
2019-03-06 18:41:11 +0200

    more debug

--- postgresql.sh
+++ postgresql.sh
@@ -4,8 +4,11 @@ function postgresql_create_db () {
 	local NEW_DB_NAME="$1"
 	# check db exist and if not - create
 	echo "Creating DB: $NEW_DB_NAME"
-	PGPASSWORD="$POSTGRESQL_PASS" psql -h "$POSTGRESQL_HOST" -p "$POSTGRESQL_PORT" -U "$POSTGRESQL_USER" -w -lqt | cut -d \| -f 1 | grep -qw "$NEW_DB_NAME" || \
+	if PGPASSWORD="$POSTGRESQL_PASS" psql -h "$POSTGRESQL_HOST" -p "$POSTGRESQL_PORT" -U "$POSTGRESQL_USER" -w -lqt | cut -d \| -f 1 | grep -qw "$NEW_DB_NAME"; then
+		echo "DB $NEW_DB_NAME already exists"
+	else
 		PGPASSWORD="$POSTGRESQL_PASS" createdb -h "$POSTGRESQL_HOST" -p "$POSTGRESQL_PORT" -U "$POSTGRESQL_USER" -w "$NEW_DB_NAME"
+	fi
 }
 
 function postgresql_grant_all_privileges_on_db () {
