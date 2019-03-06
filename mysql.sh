2200e09a7fc2f74edd9576060b7c15573c9fc7bd
Eugene Antonov <jetman@sysadm.ws>
2019-03-06 20:16:33 +0200

    sql syntax fix

--- mysql.sh
+++ mysql.sh
@@ -8,7 +8,7 @@ function mysql_create_db () {
 function mysql_grant_all_privileges_on_db () {
 	local DB_NAME="$1"
 	local USER_NAME="$2"
-	echo 'GRANT ALL PRIVILEGES ON DATABASE `'$DB_NAME'`.* TO `'$USER_NAME'`@`%`;' | mysql -u "$MYSQL_USER" --password="$MYSQL_PASS" -h "$MYSQL_HOST" -P "$MYSQL_PORT"
+	echo 'GRANT ALL PRIVILEGES ON `'$DB_NAME'`.* TO `'$USER_NAME'`@`%`;' | mysql -u "$MYSQL_USER" --password="$MYSQL_PASS" -h "$MYSQL_HOST" -P "$MYSQL_PORT"
 }
 
 function mysql_db_sanitize () {
