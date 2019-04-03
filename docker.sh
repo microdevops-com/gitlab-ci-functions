09bc2d0453fc4574063d1cac6161deb4e6de05f7
Eugene Antonov <jetman@sysadm.ws>
2019-04-03 12:40:21 +0300

    typo fix

--- docker.sh
+++ docker.sh
@@ -25,9 +25,9 @@ function docker_build_file () {
 }
 
 function docker_build_dir_args () {
-	pushd $2 && docker build --pull -t $1 --build-arg CI_COMMIT_REF_SLUG=$3 . && popd
+	pushd $2 && docker build --pull -t $1 $3 . && popd
 }
 
 function docker_build_file_args () {
-	docker build --pull -t $1 -f $2 --build-arg CI_COMMIT_REF_SLUG=$3 .
+	docker build --pull -t $1 -f $2 $3 .
 }
