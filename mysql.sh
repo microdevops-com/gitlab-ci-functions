d4da59c4784f52f48fe0752d74543e8b8ff96d51
Eugene Antonov <jetman@sysadm.ws>
2019-04-11 12:01:27 +0300

    mysql_truncate_table, mysql_query_from_file added

--- mysql.sh
+++ mysql.sh
@@ -2,7 +2,19 @@
 
 function mysql_create_db () {
 	local NEW_DB_NAME="$1"
-	echo 'CREATE DATABASE IF NOT EXISTS `'$NEW_DB_NAME'`' | mysql -u "$MYSQL_USER" --password="$MYSQL_PASS" -h "$MYSQL_HOST" -P "$MYSQL_PORT"
+	echo 'CREATE DATABASE IF NOT EXISTS `'$NEW_DB_NAME'`;' | mysql -u "$MYSQL_USER" --password="$MYSQL_PASS" -h "$MYSQL_HOST" -P "$MYSQL_PORT"
+}
+
+function mysql_truncate_table () {
+	local DB_NAME="$1"
+	local TABLE="$2"
+	echo 'TRUNCATE TABLE `'$TABLE'`;' | mysql -u "$MYSQL_USER" --password="$MYSQL_PASS" -h "$MYSQL_HOST" -P "$MYSQL_PORT" "$DB_NAME"
+}
+
+function mysql_query_from_file () {
+	local DB_NAME="$1"
+	local FILE="$2"
+	cat "$FILE" | mysql -u "$MYSQL_USER" --password="$MYSQL_PASS" -h "$MYSQL_HOST" -P "$MYSQL_PORT" "$DB_NAME"
 }
 
 function mysql_grant_all_privileges_on_db () {
