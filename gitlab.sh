1faa1aa35657d63555e58b9883e4bdae22e9481a
Eugene Antonov <jetman@sysadm.ws>
2019-06-26 18:32:35 +0300

    trigger echo curl

--- gitlab.sh
+++ gitlab.sh
@@ -13,6 +13,8 @@ function gitlab_trigger_pipeline_and_wait_success () {
 	local VARIABLES="$4"
 	local PRIVATE_TOKEN="$5"
 	
+	# Echo curl command
+	echo curl --request POST --form token=$CI_JOB_TOKEN --form ref=$REF $VARIABLES $GITLAB_URL/api/v4/projects/$PROJECT_ID/trigger/pipeline
 	# Trigger pipeline and save output
 	CURL_OUT=$(curl --request POST --form token=$CI_JOB_TOKEN --form ref=$REF $VARIABLES $GITLAB_URL/api/v4/projects/$PROJECT_ID/trigger/pipeline)
 	# Debug output
