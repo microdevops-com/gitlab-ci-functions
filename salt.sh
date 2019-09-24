c708924e877e2f51d05ba16b1ad81e070f0a5cbd
Eugene Antonov <jetman@sysadm.ws>
2019-09-24 18:09:48 +0300

    salt deploy optional deploy exec

--- salt.sh
+++ salt.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 function salt_call_deploy () {
-	local DEPLOY_SERVER=$1
-	local DEPLOY_IMAGE=$2
-	local DEPLOY_APP=$3
+	local DEPLOY_SERVER="$1"
+	local DEPLOY_IMAGE="$2"
+	local DEPLOY_APP="$3"
+
 	ssh -o BatchMode=yes -o StrictHostKeyChecking=no root@${DEPLOY_SERVER} 'docker login -u "'${CI_REGISTRY_USER}'" -p "'${CI_JOB_TOKEN}'" "'${CI_REGISTRY}'"'
 	ssh -o BatchMode=yes -o StrictHostKeyChecking=no root@${DEPLOY_SERVER} 'docker pull '${DEPLOY_IMAGE}''
-	ssh -o BatchMode=yes -o StrictHostKeyChecking=no root@${DEPLOY_SERVER} 'salt-call state.apply app.docker pillar='\''{"app": {"docker": {"deploy_only": ["'${DEPLOY_APP}'"], "apps": {"'${DEPLOY_APP}'": {"image": "'${DEPLOY_IMAGE}'"}}}}}'\'''
+	if [ -z "$4" ]; then
+		ssh -o BatchMode=yes -o StrictHostKeyChecking=no root@${DEPLOY_SERVER} 'salt-call state.apply app.docker pillar='\''{"app": {"docker": {"deploy_only": ["'${DEPLOY_APP}'"], "apps": {"'${DEPLOY_APP}'": {"image": "'${DEPLOY_IMAGE}'"}}}}}'\'''
+	else
+		local DEPLOY_EXEC="$4"
+		ssh -o BatchMode=yes -o StrictHostKeyChecking=no root@${DEPLOY_SERVER} 'salt-call state.apply app.docker pillar='\''{"app": {"docker": {"deploy_only": ["'${DEPLOY_APP}'"], "apps": {"'${DEPLOY_APP}'": {"image": "'${DEPLOY_IMAGE}'", "exec_after_deploy": "'${DEPLOY_EXEC}'"}}}}}'\'''
+	fi
 }
