13457289874ce41deec5212f26db96b7aee3ad94
Eugene Antonov <jetman@sysadm.ws>
2019-09-25 16:27:26 +0300

    ssh_copy_from_docker func added

--- misc.sh
+++ misc.sh
@@ -14,3 +14,15 @@ function ssh_salt_call_app_docker () {
 		ssh -o BatchMode=yes -o StrictHostKeyChecking=no root@${DEPLOY_SERVER} 'salt-call state.apply app.docker pillar='\''{"app": {"docker": {"deploy_only": ["'${DEPLOY_APP}'"], "apps": {"'${DEPLOY_APP}'": {"image": "'${DEPLOY_IMAGE}'", "exec_after_deploy": "'${DEPLOY_EXEC}'"}}}}}'\'''
 	fi
 }
+
+function ssh_copy_from_docker () {
+	local DEPLOY_SERVER="$1"
+	local DEPLOY_IMAGE="$2"
+	local DEPLOY_SOURCE="$3"
+	local DEPLOY_TARGET="$4"
+	local DEPLOY_STRIP="$5"
+
+	ssh -o BatchMode=yes -o StrictHostKeyChecking=no root@${DEPLOY_SERVER} 'docker login -u "'${CI_REGISTRY_USER}'" -p "'${CI_JOB_TOKEN}'" "'${CI_REGISTRY}'"'
+	ssh -o BatchMode=yes -o StrictHostKeyChecking=no root@${DEPLOY_SERVER} 'docker pull '${DEPLOY_IMAGE}''
+	ssh -o BatchMode=yes -o StrictHostKeyChecking=no root@${DEPLOY_SERVER} 'DEPLOY_TMP_CONTAINER=$(docker create '${DEPLOY_IMAGE}'); docker export ${DEPLOY_TMP_CONTAINER} | tar -C '${DEPLOY_TARGET}' --strip-components='${DEPLOY_STRIP}' -xf - '${DEPLOY_SOURCE}'; docker rm ${DEPLOY_TMP_CONTAINER}'
+}
