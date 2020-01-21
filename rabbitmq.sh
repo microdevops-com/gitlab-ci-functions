37b44c7b2ca309c29d1bd959fed50cefad9f8501
Eugene Antonov <jetman@sysadm.ws>
2020-01-21 15:37:36 +0200

    rabbitmq_purge_queue added

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-RABBITMQADMIN="rabbitmqadmin -s -H $RABBITMQ_HOST -P $RABBITMQ_MANAGEMENT_PORT -u $RABBITMQ_MANAGEMENT_USER -p $RABBITMQ_MANAGEMENT_PASS -V /"
+RABBITMQADMIN="rabbitmqadmin -s -H $RABBITMQ_HOST -P $RABBITMQ_MANAGEMENT_PORT -u $RABBITMQ_MANAGEMENT_USER -p $RABBITMQ_MANAGEMENT_PASS"
 
 function rabbitmq_create_vhost () {
-	$RABBITMQADMIN declare vhost name=$1
-	$RABBITMQADMIN declare permission vhost=$1 user=root "configure=.*" "write=.*" "read=.*"
+	$RABBITMQADMIN -V / declare vhost name=$1
+	$RABBITMQADMIN -V / declare permission vhost=$1 user=root "configure=.*" "write=.*" "read=.*"
 }
 
 function rabbitmq_add_permission () {
-	$RABBITMQADMIN declare permission vhost=$1 user=$2 "configure=.*" "write=.*" "read=.*"
+	$RABBITMQADMIN -V / declare permission vhost=$1 user=$2 "configure=.*" "write=.*" "read=.*"
 }
 
 function rabbitmq_add_read_permission () {
-	$RABBITMQADMIN declare permission vhost=$1 user=$2 "configure=''" "write=''" "read=.*"
+	$RABBITMQADMIN -V / declare permission vhost=$1 user=$2 "configure=''" "write=''" "read=.*"
 }
 
 function rabbitmq_vhost_sanitize () {
@@ -22,4 +22,10 @@ function rabbitmq_vhost_sanitize () {
 		local LENGTH="$2"
 	fi
 	echo $1 | tr "[:upper:]" "[:lower:]" | sed "s/[^a-zA-Z0-9-]/-/g" | head -c $LENGTH | sed "s/-$//g" | tr -d '\n' | tr -d '\r'
+}
+
+function rabbitmq_purge_queue () {
+	VHOST=$1
+	QUEUE=$2
+	$RABBITMQADMIN -V $VHOST purge queue name=$QUEUE
 }
