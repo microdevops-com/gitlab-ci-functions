afc589af59421a716fe072dfb8b38db683bd2e5b
Eugene Antonov <jetman@sysadm.ws>
2020-04-22 16:24:56 +0300

    mysql with connection counterparts

--- mysql.sh
+++ mysql.sh
@@ -56,3 +56,84 @@ function mysql_select_count_from_table_where () {
 	local WHERE="$3"
 	echo 'SELECT COUNT(*) FROM `'$TABLE'` WHERE '$WHERE';' | mysql -s -r -u "$MYSQL_USER" --password="$MYSQL_PASS" -h "$MYSQL_HOST" -P "$MYSQL_PORT" "$DB_NAME"
 }
+
+# with connection counterparts
+function mysql_create_db_with_connect () {
+	local NEW_DB_NAME="$1"
+	local MYSQL_USER="$2"
+	local MYSQL_PASS="$3"
+	local MYSQL_HOST="$4"
+	local MYSQL_PORT="$5"
+	echo 'CREATE DATABASE IF NOT EXISTS `'$NEW_DB_NAME'`;' | mysql -vv -u "$MYSQL_USER" --password="$MYSQL_PASS" -h "$MYSQL_HOST" -P "$MYSQL_PORT"
+}
+
+function mysql_truncate_db_with_connect () {
+	local DB_NAME="$1"
+	local MYSQL_USER="$2"
+	local MYSQL_PASS="$3"
+	local MYSQL_HOST="$4"
+	local MYSQL_PORT="$5"
+	mysql -u "$MYSQL_USER" --password="$MYSQL_PASS" -h "$MYSQL_HOST" -P "$MYSQL_PORT" -Nse 'show tables' "$DB_NAME" | while read table; do mysql -vv -u "$MYSQL_USER" --password="$MYSQL_PASS" -h "$MYSQL_HOST" -P "$MYSQL_PORT" -e "DROP TABLE $table" "$DB_NAME"; done
+}
+
+function mysql_truncate_table_with_connect () {
+	local DB_NAME="$1"
+	local TABLE="$2"
+	local MYSQL_USER="$3"
+	local MYSQL_PASS="$4"
+	local MYSQL_HOST="$5"
+	local MYSQL_PORT="$6"
+	echo 'TRUNCATE TABLE `'$TABLE'`;' | mysql -vv -u "$MYSQL_USER" --password="$MYSQL_PASS" -h "$MYSQL_HOST" -P "$MYSQL_PORT" "$DB_NAME"
+}
+
+function mysql_query_from_file_with_connect () {
+	local DB_NAME="$1"
+	local FILE="$2"
+	local MYSQL_USER="$3"
+	local MYSQL_PASS="$4"
+	local MYSQL_HOST="$5"
+	local MYSQL_PORT="$6"
+	cat "$FILE" | mysql -vv -u "$MYSQL_USER" --password="$MYSQL_PASS" -h "$MYSQL_HOST" -P "$MYSQL_PORT" "$DB_NAME"
+}
+
+function mysql_query_from_file_no_verbose_with_connect () {
+	local DB_NAME="$1"
+	local FILE="$2"
+	local MYSQL_USER="$3"
+	local MYSQL_PASS="$4"
+	local MYSQL_HOST="$5"
+	local MYSQL_PORT="$6"
+	cat "$FILE" | mysql -u "$MYSQL_USER" --password="$MYSQL_PASS" -h "$MYSQL_HOST" -P "$MYSQL_PORT" "$DB_NAME"
+}
+
+function mysql_grant_all_privileges_on_db_with_connect () {
+	local DB_NAME="$1"
+	local USER_NAME="$2"
+	local MYSQL_USER="$3"
+	local MYSQL_PASS="$4"
+	local MYSQL_HOST="$5"
+	local MYSQL_PORT="$6"
+	echo 'GRANT ALL PRIVILEGES ON `'$DB_NAME'`.* TO `'$USER_NAME'`@`%`;' | mysql -vv -u "$MYSQL_USER" --password="$MYSQL_PASS" -h "$MYSQL_HOST" -P "$MYSQL_PORT"
+}
+
+function mysql_delete_from_table_where_with_connect () {
+	local DB_NAME="$1"
+	local TABLE="$2"
+	local WHERE="$3"
+	local MYSQL_USER="$4"
+	local MYSQL_PASS="$5"
+	local MYSQL_HOST="$6"
+	local MYSQL_PORT="$7"
+	echo 'DELETE FROM `'$TABLE'` WHERE '$WHERE';' | mysql -vv -u "$MYSQL_USER" --password="$MYSQL_PASS" -h "$MYSQL_HOST" -P "$MYSQL_PORT" "$DB_NAME"
+}
+
+function mysql_select_count_from_table_where_with_connect () {
+	local DB_NAME="$1"
+	local TABLE="$2"
+	local WHERE="$3"
+	local MYSQL_USER="$4"
+	local MYSQL_PASS="$5"
+	local MYSQL_HOST="$6"
+	local MYSQL_PORT="$7"
+	echo 'SELECT COUNT(*) FROM `'$TABLE'` WHERE '$WHERE';' | mysql -s -r -u "$MYSQL_USER" --password="$MYSQL_PASS" -h "$MYSQL_HOST" -P "$MYSQL_PORT" "$DB_NAME"
+}
