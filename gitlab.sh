1159cb7b3fd18c9867dce2a252d4973471a8e1a2
Eugene Antonov <jetman@sysadm.ws>
2021-01-20 18:54:06 +0200

    more gitlab funcs

--- gitlab.sh
+++ gitlab.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 function gitlab_trigger_pipeline_and_wait_success () {
+	# !!!
+	# This func is not needed anymore. Use parent-child pipelines with "strategy: depend".
+
 	# While CI_JOB_TOKEN or trigger token allow you to trigger pipeline, they do not allow to get status of the pipeline.
 	# So you need to use private token with api instead - insecure but no other way yet:
 	# https://gitlab.com/gitlab-org/gitlab-ce/issues/39640 .
@@ -68,4 +71,43 @@ function clean_build_dir () {
 			find "${CI_PROJECT_DIR}" -mindepth 1 -maxdepth 1 -print0 | xargs -0 rm -rf
 		fi
 	fi
+}
+
+function runner_resource_lock () {
+	# Resource Groups work only within one project. When using multiple projects with the same non-concurrant resource additional locks needed.
+	# https://docs.gitlab.com/ee/ci/yaml/#resource_group
+	# https://gitlab.com/gitlab-org/gitlab/-/issues/122010
+	# https://gitlab.com/gitlab-org/gitlab/-/issues/39057 (13.9 milestone expected)
+	# For better cross project cross runner locks something like https://github.com/joanvila/aioredlock should be used.
+
+	# In case of job cancellation lock will not be removed. after_job will help as well - it is not runned on cancellation.
+	# So wait for timeout and remove lock with no error. Let the non-concurrant code report the error.
+
+	local RESOURCE_NAME="$1"
+	local RESOURCE_LOCK_DIR="${HOME}/.resource_lock"
+	local RESOURCE_LOCK_LOCK_DIR="${RESOURCE_LOCK_DIR}/${RESOURCE_NAME}.lock"
+	local RESOURCE_LOCK_LOCK_RETRIES=1
+	local RESOURCE_LOCK_LOCK_RETRIES_MAX=$2
+
+	mkdir -p ${RESOURCE_LOCK_DIR}
+	echo "NOTICE: Waiting for lock ${RESOURCE_LOCK_LOCK_DIR}: "
+	until mkdir "${RESOURCE_LOCK_LOCK_DIR}" || (( RESOURCE_LOCK_LOCK_RETRIES == RESOURCE_LOCK_LOCK_RETRIES_MAX )); do
+		echo -n "."
+		let "RESOURCE_LOCK_LOCK_RETRIES++"
+		sleep 1
+	done
+	if [ ${RESOURCE_LOCK_LOCK_RETRIES} -eq ${RESOURCE_LOCK_LOCK_RETRIES_MAX} ]; then
+		echo "WARNING: Cannot acquire lock after ${RESOURCE_LOCK_LOCK_RETRIES}s, ignoring lock on ${RESOURCE_LOCK_LOCK_DIR} by timeout"
+	else
+		echo "NOTICE: Successfully acquired lock on ${RESOURCE_LOCK_LOCK_DIR}"
+	fi
+}
+
+function runner_resource_unlock () {
+	local RESOURCE_NAME="$1"
+	local RESOURCE_LOCK_DIR="${HOME}/.resource_lock"
+	local RESOURCE_LOCK_LOCK_DIR="${RESOURCE_LOCK_DIR}/${RESOURCE_NAME}.lock"
+
+	rm -rf "${RESOURCE_LOCK_LOCK_DIR}"
+	echo "NOTICE: Successfully removed lock on ${RESOURCE_LOCK_LOCK_DIR}"
 }
