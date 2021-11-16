793af16428c8df3bf2fc0e24d6c1b32fc571f0d4
Andru Cherny <wiroatom@gmail.com>
2021-11-16 15:56:27 +0200

    add backward compatibility

--- kubernetes.sh
+++ kubernetes.sh
@@ -28,8 +28,8 @@ function kubectl_namespace {
 function rancher_login {
   if [ -z "$RANCHER_SERVER" ] && [ -z "$RANCHER_TOKEN" ] ; then
     $RANCHER login --token "$KUBE_TOKEN" "$KUBE_SERVER"
-	else
-	  $RANCHER login --token "$RANCHER_TOKEN" "$RANCHER_SERVER"
+  else
+    $RANCHER login --token "$RANCHER_TOKEN" "$RANCHER_SERVER"
   fi
 }
 
@@ -37,10 +37,10 @@ function rancher_login_project {
   if [ -z "$RANCHER_SERVER" ] && [ -z "$RANCHER_TOKEN" ] ; then
     # Deprecated logic
     local RANCHER_PROJECT_NAME="$RANCHER_PROJECT"
-	  local RANCHER_PROJECT_ID=$(echo "" | $RANCHER login --token "$KUBE_TOKEN" "$KUBE_SERVER" 2>/dev/null | grep -E "local\:p-[[:alnum:]]+[[:space:]]+${RANCHER_PROJECT_NAME}" | awk '{print $3}')
+    local RANCHER_PROJECT_ID=$(echo "" | $RANCHER login --token "$KUBE_TOKEN" "$KUBE_SERVER" 2>/dev/null | grep -E "local\:p-[[:alnum:]]+[[:space:]]+${RANCHER_PROJECT_NAME}" | awk '{print $3}')
     $RANCHER login --token "$KUBE_TOKEN" --context "$RANCHER_PROJECT_ID" "$KUBE_SERVER"
-	else
-	  $RANCHER login "$RANCHER_SERVER" --token "$RANCHER_TOKEN" --context "$RANCHER_CLUSTER_ID:$RANCHER_PROJECT_ID"
+  else
+    $RANCHER login "$RANCHER_SERVER" --token "$RANCHER_TOKEN" --context "$RANCHER_CLUSTER_ID:$RANCHER_PROJECT_ID"
   fi
 }
 
@@ -120,16 +120,16 @@ function namespace_secret_acme_cert () {
 }
 
 function helm_cluster_login {
-	mkdir -p $PWD/.helm
-	if [ -z "$RANCHER_SERVER" ] && [ -z "$RANCHER_TOKEN" ] ; then
-	  KUBECONFIG=$PWD/.helm/cluster.yml kubectl config set-cluster remote-cluster --server=$KUBE_SERVER
+  mkdir -p $PWD/.helm
+  if [ -z "$RANCHER_SERVER" ] && [ -z "$RANCHER_TOKEN" ] ; then
+    KUBECONFIG=$PWD/.helm/cluster.yml kubectl config set-cluster remote-cluster --server=$KUBE_SERVER
     KUBECONFIG=$PWD/.helm/cluster.yml kubectl config set-credentials user-gvnrn --token=$KUBE_TOKEN
     KUBECONFIG=$PWD/.helm/cluster.yml kubectl config set-context remote-cluster --user=user-gvnrn --cluster=remote-cluster
     KUBECONFIG=$PWD/.helm/cluster.yml kubectl config use-context remote-cluster
-	else
+  else
     $RANCHER cluster kubeconfig ${RANCHER_CLUSTER_ID}> $PWD/.helm/cluster.yml
-	fi
-	chmod 600 $PWD/.helm/cluster.yml
+  fi
+  chmod 600 $PWD/.helm/cluster.yml
 }
 
 # We shouldn't leave credentials in the workspace as they may change
